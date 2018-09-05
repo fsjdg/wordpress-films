2e6307d9facf3f116f2fb3bb0229d641ddabc815
Freddy Delgdo G <fdelgado@MBP-de-Freddy.home>
2018-09-05 11:49:54 +0200

    Added new post type Film

--- wp-content/themes/unite-child/functions.php
+++ wp-content/themes/unite-child/functions.php
@@ -2,6 +2,23 @@
 
 // Import parent styles
 add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles', PHP_INT_MAX);
+
 function enqueue_child_theme_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
+
+// Adding new post type
+function create_posttype() {
+    register_post_type( 'films',
+        array(
+            'labels' => array(
+                'name' => __( 'Films' ),
+                'singular_name' => __( 'Film' )
+            ),
+            'public' => true,
+            'rewrite' => array('slug' => 'films'),
+        )
+    );
+}
+
+add_action( 'init', 'create_posttype' );
